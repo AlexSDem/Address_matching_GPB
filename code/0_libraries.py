b0d7cb17065ca214de2c1e98095a4aa66e65d2ee
AlexSDem <sanya131997@mail.ru>
2026-01-21 20:27:10 +0300

    Update 0_libraries.py

--- code/0_libraries.py
+++ code/0_libraries.py
@@ -2,7 +2,3 @@
 
 # СТАБИЛЬНЫЙ набор для baseline
 !pip -q install "osmnx>=1.8" "geopandas>=0.14" "shapely>=2.0" "scikit-learn>=1.3" "rapidfuzz>=3.0" "nlpaug>=1.1.11"
-
-# 4. ставим geocoder
-!pip install reverse_geocoder
-!pip install natasha
